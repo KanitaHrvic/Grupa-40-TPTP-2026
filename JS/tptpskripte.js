8034dcd93fa927c2a2982bd73c1abc7beb167809
Kenan Ibeljic <kenan.ibeljic@fet.ba>
2026-05-14 22:24:57 +0200

    izmjene u css i html i dodana js skripta

---
+++ JS/tptpskripte.js
@@ -0,0 +1,76 @@
+//---------------------------------vvvv SKRIPTA ZA KONTAKT FORMU vvv --------------------------------------------------------
+const forma = document.getElementById('kontakt-forma');
+forma.addEventListener('submit', function(event){
+event.preventDefault();
+let jeIspravno = true;
+const ime = document.getElementById('Ime');
+const prezime = document.getElementById('Prezime');
+const email = document.getElementById('email');
+const telefon = document.getElementById('telefon');
+const tema = document.getElementById('tema');
+const poruka = document.getElementById('poruka');
+const imeGreska = document.getElementById('Ime-greska');
+const prezimeGreska = document.getElementById('Prezime-greska');
+const emailGreska = document.getElementById('email-greska');
+const telefonGreska = document.getElementById('telefon-greska');
+const temaGreska = document.getElementById('tema-greska');
+const porukaGreska = document.getElementById('poruka-greska');
+ukloniGresku (ime, imeGreska);
+if (ime.value.trim() === ''){
+    prikaziGresku (ime, imeGreska, 'Molimo popunite ovo polje');
+    jeIspravno = false;
+}
+ukloniGresku(prezime, prezimeGreska);
+if( prezime.value.trim() === ''){
+    prikaziGresku (prezime, prezimeGreska, 'Molimo popunite ovo polje');
+    jeIspravno = false;
+}
+ukloniGresku (email, emailGreska);
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+if (email.value.trim() === ''){
+    prikaziGresku (email, emailGreska, 'Molimo popunite ovo polje');
+    jeIspravno = false;
+}else if(!emailRegex.test(email.value)){
+    prikaziGresku(email, emailGreska, 'Molimo ispravno upisite mail');
+    jeIspravno = false;
+}
+ukloniGresku (telefon, telefonGreska);
+const telefonRegex = /^[0-9\s\-]+$/;
+if(telefon.value.trim() === ''){
+    prikaziGresku(telefon, telefonGreska, 'Molimo popunite ovo polje');
+    jeIspravno = false;
+}else if(!telefonRegex.test(telefon.value)){
+    prikaziGresku (telefon, telefonGreska, 'Molimo ispravno unesite broj telefona');
+    jeIspravno = false;
+}
+ukloniGresku (tema, temaGreska);
+if(tema.value === ''){
+    prikaziGresku (tema, temaGreska, 'Molimo izaberite temu upita');
+    jeIspravno = false
+}
+ukloniGresku (poruka, porukaGreska);
+if (poruka.value.trim() === ''){
+    prikaziGresku (poruka, porukaGreska, 'Molimo unesite poruku');
+    jeIspravno = false;
+}
+if(jeIspravno){
+    const uspjesna = document.getElementById('uspjesna-poruka');
+    uspjesna.textContent = 'Hvala '+ ime.value + '! Vaša poruka je poslana';
+    uspjesna.style.display = 'block';
+    forma.reset(); 
+}
+})
+document.getElementById('reset-dugme').addEventListener('click', function(){
+    const uspjesna = document.getElementById('uspjesna-poruka');
+    uspjesna.textContent = '';
+    uspjesna.style.display = 'none';
+})
+function prikaziGresku(polje, span, poruka){
+span.textContent = poruka;
+polje.parentElement.classList.add('polje-greska');
+}
+function ukloniGresku (polje, span){
+    span.textContent = '';
+    polje.parentElement.classList.remove('polje-greska');
+}
+//------------------------------------------^^^ SKRIPTA ZA KONTAKT FORMU ^^^---------------------------------------------
